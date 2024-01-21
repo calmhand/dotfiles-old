2df43ba4f132426257d9c139d97231f0223de070
johnny <johnny.goulb@gmail.com>
2024-01-21 01:54:24 -0500

    small changes

--- lua/remontalo/remap.lua
+++ lua/remontalo/remap.lua
@@ -3,4 +3,4 @@ vim.g.mapleader = " "
 -- While in normal mode, pressing "leader pv", the explore window will open.
 -- The "leader" in this case is "space."
 vim.keymap.set("n", "<leader>pv", vim.cmd.NvimTreeOpen)
-vim.keymap.set("n", "<leader>pt", vim.cmd.NvimTreeToggle)
+vim.keymap.set("n", "<leader>pb", vim.cmd.NvimTreeToggle)
