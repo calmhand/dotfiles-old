2e3b50de50252af811c123a1e1761522fba29bf1
johnny <johnny.goulb@gmail.com>
2024-05-24 15:57:32 -0400

    -extra lsp plugins; +updating plugins

--- lua/remontalo/lazy.lua
+++ lua/remontalo/lazy.lua
@@ -35,10 +35,6 @@ require("lazy").setup({
 
   {'williamboman/mason.nvim'}, -- LSP + Helpers
   {'williamboman/mason-lspconfig.nvim'},
-  {'VonHeikemen/lsp-zero.nvim', branch = 'v3.x'},
   {'neovim/nvim-lspconfig'},
-  {'hrsh7th/cmp-nvim-lsp'},
-  {'hrsh7th/nvim-cmp'},
-  {'L3MON4D3/LuaSnip'},
 
 })
